d8d49e385c8e051972a4535a00b6df63be7f3151
d-salt <d.salt.web+dev@gmail.com>
2019-11-19 10:13:26 +0900

    add comment output

--- split2pdfs.sh
+++ split2pdfs.sh
@@ -11,9 +11,11 @@ function croppdf () {
     target_height=$org_height
 
     # crop left page
+    echo '=> Crop left harf page'
     pdftocairo $file -pdf -nocrop -x 0 -y 0 -W $target_width -H $target_height -paperw $target_height -paperh $target_width ${input_file_name}/cropped/${file_name}.left.pdf
 
     # crop right page
+    echo '=> Crop left harf page'
     pdftocairo $file -pdf -nocrop -x $target_width -y 0 -W $target_width -H $target_height -paperw $target_height -paperh $target_width ${input_file_name}/cropped/${file_name}.right.pdf
 }
 
@@ -30,9 +32,11 @@ output_file=${input_file_name}.split.pdf
 # set working dir at the same dir with input_file
 cd $(dirname $input_file)
 # make working dir
+echo '=> Create work directory'
 mkdir -p ${input_file_name}/split && mkdir -p ${input_file_name}/cropped && mkdir -p ${input_file_name}/united
 
 # separate each page
+echo '=> Split each pages'
 pdfseparate $input_file ${input_file_name}/split/%d.pdf
 
 # crop each page
@@ -42,6 +46,7 @@ do
         croppdf $pdf
         name=$(basename $pdf)
         number=${name%.*}
+        echo '=> Combine left and right page'
         pdfunite $(ls ${input_file_name}/cropped/${number}.* | sort -n) ${input_file_name}/united/${number}.pdf
     else
         continue
